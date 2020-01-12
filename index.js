4b7e05bc6903557862700ecba226cd91ea98bc37
Your Name <you@example.com>
2020-01-12 13:22:25 -0500

    new project

--- index.js
+++ index.js
@@ -3,11 +3,6 @@ const fs = require('fs');
 const parser = require('./parser');
 const configTracker = require('./configtracker');
 const modules = fs.readdirSync('./modules');
-require('dotenv/config');
-const http = require('http');
-const port = process.env.PORT || 3000;
-// this is a simple server 
-http.createServer().listen(port);
 
 const client = new Discord.Client();
 const dispatch = new parser();
@@ -31,8 +26,4 @@ client.on('message', (msg) => {
     dispatch.informModules(msg);
 });
 
-bot.on ('error', err => {
-    console.log(err);
-})
-
-client.login(config.get('process.env.TOKEN'));
+client.login(config.get('bot-token'));  
